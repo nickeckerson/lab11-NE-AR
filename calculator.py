1203dc015487e970f4b9458106cfcf0f3a4c089d
albertoramirez77 <spn8wntwh2@privaterelay.appleid.com>
2026-04-09 11:12:23 -0400

    fixed again

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
-# https://github.com/nickeckerson/lab11-NE-AR
-# Nicholas Eckerson
-# Alberto Ramirez
+"""https://github.com/nickeckerson/lab11-NE-AR.git
+Partner 1: Nick Eckerson
+Partner 2: Alberto Ramirez-Aguiar"""
 
 import math
 
@@ -30,7 +30,6 @@ def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Invalid logarithm input")
     return math.log(b, a)
-
 
 def exponent(a, b):
     return a ** b
