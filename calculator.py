22465c1e5fed6147cc19fd6b1768954a5757f831
albertoramirez77 <spn8wntwh2@privaterelay.appleid.com>
2026-04-09 10:54:15 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -25,13 +25,6 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
-
-def divide(a, b):
-    if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero")
-    return b / a
-
-
 def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Invalid logarithm input")
@@ -41,37 +34,4 @@ def logarithm(a, b):
 def exponent(a, b):
     return a ** b
 
-
-<<<<<<< HEAD
-=======
-One function per operation, in order.
-
-Partner 1: Nick Eckerson
-Partner 2: Alberto Ramirez-Aguiar
-https://github.com/nickeckerson/lab11-NE-AR.git
-
-
-"""
-# First example
-import math
-def add(a, b):
-    return a + b
->>>>>>> 9f1f860157bca0f0f51846bbe8d9f683cefc680c
-
-def sub(a, b):
-    return a - b
-
-def mul(a, b):
-    return a * b
-
-def div(a, b):
-    if a == 0:
-        raise ZeroDivisionError
-    else: return b / a
-
-def log(a, b):
-    return math.log(b, a)
-
-def exp(a, b):
-    return a ** b
 
