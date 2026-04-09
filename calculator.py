c8a4de49eee7c4724da4a8bf29e41b6d72ab6f80
Nicholas Eckerson <you@example.com>
2026-04-09 10:39:36 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -1,11 +1,46 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/nickeckerson/lab11-NE-AR
+# Nicholas Eckerson
+# Alberto Ramirez
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of negative number")
+    return math.sqrt(a)
+
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+
+def logarithm(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid logarithm input")
+    return math.log(b, a)
+
+
+def exponent(a, b):
+    return a ** b
+
+
 
 
